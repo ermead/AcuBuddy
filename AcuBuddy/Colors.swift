7caff9ab0bdf6bb2a2df0209eecc45e8a7e55e65
eric mead <ermead@vt.edu>
2015-12-16 18:22:21 -0700

    initial Commit

---
+++ AcuBuddy/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  AcuBuddy
+//
+//  Created by Eric Mead on 12/16/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let blue = UIColor(red: 46.0 / 255.0, green: 117.0 / 255.0, blue: 146.0 / 255.0, alpha: 1.0)
+    static let red = UIColor(red: 209.0 / 255.0, green: 42.0 / 255.0, blue: 24.0 / 255.0, alpha: 1.0)
+    static let white = UIColor.whiteColor()
+    static let clear = UIColor.clearColor()
+}
+
